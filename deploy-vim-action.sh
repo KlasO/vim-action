5b79fd06cc5a9a42507d4c747e07f5786a703fb0
Klas Olsson <klas.olsson@memoratus.se>
2021-12-20 20:30:47 +0100

    test if colors is fetched by iVim iplug

--- deploy-vim-action.sh
+++ deploy-vim-action.sh
@@ -4,10 +4,10 @@
 set -x
 if [ -d "~/.vim" ]
 then
-  cp $CODE_DIR/vim-action/ftdetect/* ~/.vim/ftdetect/ 
-  cp $CODE_DIR/vim-action/ftplugin/* ~/.vim/ftplugin/
-  cp $CODE_DIR/vim-action/syntax/* ~/.vim/syntax/
-  cp $CODE_DIR/vim-action/indent/* ~/.vim/indent/
+  cp -r $CODE_DIR/vim-action/ftdetect/* ~/.vim/ftdetect/ 
+  cp -r $CODE_DIR/vim-action/ftplugin/* ~/.vim/ftplugin/
+  cp -r $CODE_DIR/vim-action/syntax/* ~/.vim/syntax/
+  cp -r $CODE_DIR/vim-action/indent/* ~/.vim/indent/
 else
   mkdir -p ~/.vim/ftdetect && cp $CODE_DIR/vim-action/ftdetect/* ~/.vim/ftdetect/  || echo "ERROR!"
   mkdir -p ~/.vim/ftplugin && cp $CODE_DIR/vim-action/ftplugin/* ~/.vim/ftplugin/ || echo "ERROR!"
